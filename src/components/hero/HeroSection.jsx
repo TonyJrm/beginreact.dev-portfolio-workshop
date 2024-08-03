bd8702b8569bc32d6ff3f4f6b8334daed502d2d6
Tony JEROME <tony.jerome@include-tech.fr>
2024-08-03 14:52:36 +0200

    Hero section done

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -2,16 +2,16 @@ import { FULL_NAME } from "../../lib/config";
 
 export const HeroSection = () => {
   return (
-    <div className="relative">
+    <div className="relative flex align-middle flex-col items-center max-w-4xl m-auto md:flex-row md:ml-[42px]">
       <img
         width={300}
         height={300}
         src="/images/avatar.jpg"
         alt="avatar"
-        className="rounded shadow-lg"
+        className="top-[-16px] right-[-8px] rounded-2xl shadow-lg md:absolute"
       />
       {/* Hero - Exercise*/}
-      <div className="">
+      <div className="flex flex-col gap-4 md:relative md:mr-48">
         {/* Hero - Exercise*/}
         <h1 className="text-6xl drop-shadow-[0_0px_20px_rgba(0,0,0,0.25)] md:text-8xl">
           I'm{" "}
@@ -19,7 +19,7 @@ export const HeroSection = () => {
             {FULL_NAME}
           </span>
         </h1>
-        <p className="max-w-xl text-xl">
+        <p className="max-w-xl text-xl dark:drop-shadow-[0_0px_5px_rgba(0,0,0,1)]">
           <b>Apprenti React.</b> I’m a software developer that make thing on
           internet, very happy to see your here, place holder please fill
           something here please fill something here.
